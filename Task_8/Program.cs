15de80a3e348c60a96088f191c7ece887e7361c4
Dimitry-P <dimitry.piatnitsky@yahoo.com>
2024-12-23 16:28:40 +0300

    Add project files.

---
+++ Task_8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_8
+{
+    internal class Program
+    {
+        public static string number;
+        public static int result;
+        public static int sum = 0;
+        
+            //•	Задача 8: Напишите программу, которая вычисляет
+            //сумму чисел от 1 до N, используя цикл for.
+            static void Main(string[] args)
+        {
+            Console.WriteLine("Введите любое число больше 1");
+            number = Console.ReadLine();
+            result = int.Parse(number);
+            for (int i = 1; i < result+1; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine("Сумма чисел равна: "+sum);
+        }
+    }
+}
